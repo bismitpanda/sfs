3861d32680606328de8b933a73c2f13fe33008eb
Blood Rogue <64477767+blood-rogue@users.noreply.github.com>
2024-01-03 00:39:26 +0530

    Reverted to previous directory structure

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,11 +16,11 @@ module.exports = {
         ],
         "no-unused-vars": "off",
         "@typescript-eslint/no-unused-vars": [
-            "warn",
+            "error",
             {
-                argsIgnorePattern: "^_",
-                varsIgnorePattern: "^_",
-                caughtErrorsIgnorePattern: "^_",
+                argsIgnorePattern: "_",
+                varsIgnorePattern: "_",
+                caughtErrorsIgnorePattern: "_",
             },
         ],
     },
